7b4ac1fc67eea7bb4da5bfbd30c03c0b3ad22e82
abubalo <abzynobalogun@gmail.com>
2023-02-25 14:59:27 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="bg-blue-500 h-full">
+      <h1 className="text-red-500 font-sm">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
