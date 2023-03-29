1e22bb77c46045a00e6d1e9f4694664fb4d4e591
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-03-29 16:27:28 +0200

    Modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    outDir: '/client/build',
+    outDir: '/build',
     // ... other build options ...
   },
 })
